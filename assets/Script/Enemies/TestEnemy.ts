6187ee6d81444a906e6c517846a5e6d9a654ad64
110062305 <khrisintw@gmail.com>
2023-06-17 12:11:48 +0800

    construct node pool

--- assets/Script/Enemies/TestEnemy.ts
+++ assets/Script/Enemies/TestEnemy.ts
@@ -1,77 +1,79 @@
-import lifebar from "../Game/Lifebar";
+import lifebar from '../Game/Lifebar'
 
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator
 
 @ccclass
 export default class TestEnemy extends cc.Component {
+  isFrozen: boolean = false
 
-    isFrozen: boolean = false;
+  // LIFE-CYCLE CALLBACKS:
 
-    // LIFE-CYCLE CALLBACKS:
+  @property(cc.Node)
+  player: cc.Node = null
 
-    @property(cc.Node)
-    player: cc.Node = null;
+  @property(lifebar)
+  playerLife: lifebar = null
 
-    @property(lifebar)
-    playerLife: lifebar = null;
+  @property(Number)
+  moveSpeed: number = 50
 
-    @property(Number)
-    moveSpeed: number = 50;
+  private EnemyManager = null
 
-    private enemyManager = null;
+  onLoad() {
+    cc.director.getPhysicsManager().enabled = true
+    cc.director.getCollisionManager().enabled = true
+    this.player = cc.find('Canvas/Player')
+    this.playerLife = cc.find('Canvas/Player/lifebar').getComponent(lifebar)
+  }
 
-    onLoad () {
-        cc.director.getPhysicsManager().enabled = true;
-        cc.director.getCollisionManager().enabled = true;
-        this.player = cc.find("Canvas/Player");
-        this.playerLife = cc.find("Canvas/Player/lifebar").getComponent(lifebar);
+  start() {
+    this.isFrozen = false
+  }
+
+  update(dt) {
+    cc.log(this.playerLife.cur_life)
+    if (this.isFrozen == true) {
+      return
     }
+    //this.node.x += 40 * dt;
 
-    start () {
-        this.isFrozen = false;
+    // chase player's position
+    let playerPos = this.player.position
+    let enemyPos = this.node.position
+    let direction = playerPos.sub(enemyPos)
+    let normalizedDirection = direction.normalize()
+    this.node.position = enemyPos.add(
+      normalizedDirection.mul(this.moveSpeed * dt),
+    )
+  }
+
+  gameTick(dt) {}
+
+  public init(node: cc.Node) {
+    this.isFrozen = false
+    this.player = cc.find('Canvas/Player')
+    this.playerLife = cc.find('Canvas/Player/lifebar').getComponent(lifebar)
+    this.setInitPos(node)
+  }
+
+  setInitPos(node: cc.Node) {
+    this.node.parent = node
+
+    this.node.position = cc.v3(0, 0, 0)
+    this.moveSpeed = 50
+  }
+
+  // this function is called when the enemy manager calls "get" API.
+  reuse(EnemyManager) {
+    this.EnemyManager = EnemyManager
+  }
+
+  onBeginContact(contact, selfCollider, otherCollider) {
+    if (otherCollider.node.name == 'Player') {
+      this.playerLife.minusLife(10)
     }
-
-    update() {
-        cc.log(this.playerLife.cur_life)
+    if (otherCollider.node.name == 'Bullet') {
+      //this.node.destroy();
     }
-
-    gameTick (dt) {
-        if(this.isFrozen == true){
-            return;
-        }
-        //this.node.x += 40 * dt;
-
-        // chase player's position
-        let playerPos = this.player.position;
-        let enemyPos = this.node.position;
-        let direction = playerPos.sub(enemyPos);
-        let normalizedDirection = direction.normalize();
-        this.node.position = enemyPos.add(normalizedDirection.mul(this.moveSpeed * dt));
-    }
-
-    public init(node: cc.Node) {
-        this.isFrozen = false;
-        this.player = cc.find("Canvas/Player");
-        this.playerLife = cc.find("Canvas/Player/lifebar").getComponent(lifebar);
-        this.setInitPos(node);
-    }
-
-    setInitPos(node: cc.Node) {
-        this.node.position = cc.v3(0, 0, 0);
-    }
-
-    reuse(enemyManager)
-    {
-        this.enemyManager = enemyManager;
-    }
-        
-    onBeginContact(contact, selfCollider, otherCollider) {
-        if (otherCollider.node.name == "Player") {
-            this.playerLife.minusLife(10);
-        }
-        if (otherCollider.node.name == "Bullet") {
-            //this.node.destroy();
-        }
-    }
-
+  }
 }
--- assets/Script/EnemyManager.ts
+++ assets/Script/EnemyManager.ts
@@ -1,27 +1,33 @@
-// Learn TypeScript:
-//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
-// Learn Attribute:
-//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
-
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator
 
 @ccclass
-export default class EnemyManager extends cc.Component {
+export default class enemyManager extends cc.Component {
+  @property(cc.Prefab)
+  private enemyPrefab: cc.Prefab = null
 
-    @property(cc.Prefab)
-    enemyPrefab: cc.Prefab = null;
+  private enemyPool = null
 
-    private enemyPool: cc.NodePool = null;
+  onLoad() {
+    this.enemyPool = new cc.NodePool('TestEnemy')
 
-    onLoad() {
-        this.enemyPool = new cc.NodePool('TestEnemy');
+    let maxEnemyNum = 8
 
-        
+    for (let i: number = 0; i < maxEnemyNum; i++) {
+      let enemy = cc.instantiate(this.enemyPrefab)
+
+      this.enemyPool.put(enemy)
+      cc.log('enemyPool size: ' + this.enemyPool.size())
     }
 
-    
+    this.schedule(this.createEnemy, 1) //set one enemy to the scene every 0.5s .
+  }
 
-    
+  //call this function to add new enemy to the scene.
+  private createEnemy() {
+    let enemy = null
+
+    if (this.enemyPool.size() > 0) enemy = this.enemyPool.get(this.enemyPool)
+
+    if (enemy != null) enemy.getComponent('TestEnemy').init(this.node)
+  }
 }
