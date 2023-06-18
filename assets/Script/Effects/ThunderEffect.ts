bdc5ca45d22312fba87dce2e160b4f3a114d53a3
110062329 <liu.zack0505@gmail.com>
2023-06-18 19:45:12 +0800

    update thunder effect

--- assets/Script/Effects/ThunderEffect.ts
+++ assets/Script/Effects/ThunderEffect.ts
@@ -8,26 +8,30 @@ export default class ThunderEffect extends cc.Component {
 
     start () {
         // random rotate 45 ~ -45 degree
-        let randomRotate = Math.random() * 90 - 45;
-        this.node.angle = -randomRotate;
+        // let randomRotate = Math.random() * 90 - 45;
+        // this.node.angle = -randomRotate;
 
         // random scaleX 0.5 ~ 1.5
-        let randomScaleX = Math.random() + 0.5;
-        this.node.scaleX = randomScaleX;
+        // let randomScaleX = Math.random() + 0.5;
+        // this.node.scaleX = randomScaleX;
 
         // random opacity 0.5 ~ 1
-        let randomOpacity = Math.random() + 0.5;
-        this.node.opacity = randomOpacity * 255;
+        // let randomOpacity = Math.random() + 0.5;
+        // this.node.opacity = randomOpacity * 255;
 
         // random skewX 0 ~ 30
-        let randomSkewX = Math.random() * 30;
-        this.node.skewX = randomSkewX;
+        // let randomSkewX = Math.random() * 30;
+        // this.node.skewX = randomSkewX;
 
         // tiny adjust position randomly
         /*let randomAdjustX = Math.random() * 10 - 5;
         let randomAdjustY = Math.random() * 10 - 5;
         this.node.x += randomAdjustX;
         this.node.y += randomAdjustY;*/
+        const animation = this.node.getComponent(cc.Animation);
+        const randomIndex = Math.floor(Math.random() * 3);
+        const animationName = 'thunderEffect' + randomIndex.toString();
+        animation.play(animationName); 
         
         // destroy this node after x second
         this.scheduleOnce(function() {
