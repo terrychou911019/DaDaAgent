ffa91c2b653437ebc02134fde853f4c5b15ded02
TerryChou <terrychou911019@gmail.com>
2023-06-19 22:20:44 +0800

    enemy skin

--- assets/Script/Enemies/TestEnemy.ts
+++ assets/Script/Enemies/TestEnemy.ts
@@ -81,7 +81,7 @@ export default class TestEnemy extends cc.Component {
 			this.collider.enabled = false
 			// let fade = cc.fadeOut(1)
 			this.anim.stop()
-			this.anim.play('goblin_die')
+			this.anim.play('die')
 			this.scheduleOnce(() => {
 				this.EnemyManager.put(this.node)
 			}, 0.8)
@@ -226,7 +226,7 @@ export default class TestEnemy extends cc.Component {
 		this.isFrozen = false
 		this.isHit = false
 		this.node.opacity = 255
-		this.anim.play('goblin_walk')
+		this.anim.play('walk')
 		this.rigidbody.enabledContactListener = true
 		this.collider.enabled = true
 
